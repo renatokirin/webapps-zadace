bbfd582d657682706ed44eaa053fc5533f0d5e6c
renatokirin <renatokirin@gmail.com>
2022-10-10 14:02:00 +0200

    Rjesen zadatak 3

--- 01-zadace/01-zadaca.js
+++ 01-zadace/01-zadaca.js
@@ -6,3 +6,10 @@ function hrkToEur(hrk) { // 1 HRK = 0.132851 EUR
 
 console.log(hrkToEur(7.53));
 
+
+// 3) funkcija koja provjerava nalazi li se dani broj X unutar [100,150000]
+function unutarIntervala(x) {
+    (x >= 100 && x <= 150000) ? console.log(x + ' je unutar [100,150000]') : console.log(x + ' nije unutar [100,150000]');
+}
+
+unutarIntervala(53);
